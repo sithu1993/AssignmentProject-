b95e34e70757460f58733a3ca16e931fe09689f0
AyeSiThuAung <sithu@echoplot.com>
2017-10-09 17:25:10 +0630

    Initialized Project

---
+++ app/src/main/java/com/example/dell/myapplication/ShowListActivity.java
@@ -0,0 +1,18 @@
+package com.example.dell.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dell on 10/9/2017.
+ */
+
+public class ShowListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_list);
+    }
+}
